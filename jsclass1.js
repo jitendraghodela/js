5fc8d9b17b5b2ed31933891435270712dcd0e8c9
Jitendra Kumawat <97525452+jitendraghodela@users.noreply.github.com>
2025-04-22 04:35:01 +0000

    testt

---
+++ jsclass1.js
@@ -0,0 +1,40 @@
+// alert("hello world");
+// console.log("hello world");
+
+// ##const student = {
+//     Name: "Kishan",
+//     age: 19,
+//     class: "2st",
+//     hobby: ["criket","movies", "music"], //this is array not next!
+//     crush: "[joythi, rianshi, jitika]"// this is all string now
+
+// }
+
+// var col = student.hobby[0];
+
+// console.log(col)
+// console.log(student.age);
+// console.log(student.Name);
+// console.log(student.class);
+// console.log(student.crush);
+
+//#### we can store array in objects
+// const students = {
+//     student1: { Name: "ram", age: 18, class: "2nd" },
+//     student2: { Name: "aam", age: 19, class: "3rd" },
+//     student3: { Name: "kam", age: 20, class: "4th" }
+// }
+
+
+//examples
+// students.student1["Name"] = "kishan";
+// console.log(students.student1.Name);
+
+// console.log(students.student1.Name);
+// console.log(students.student1.age);
+// console.log(students.student1.class);
+
+
+// console.log(students.student2.Name);
+// console.log(students.student2.age);
+// console.log(students.student2.class);
